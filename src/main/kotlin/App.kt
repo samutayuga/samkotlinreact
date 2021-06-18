cef1da385f58169fc78eabff7525e7dd41fb7ebd
Putu Mas Mertayasa <putumas.mertayasa.e@thalesdigital.io>
2021-06-18 08:23:08 +0800

    chore: add missing files

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,18 +1,32 @@
-import kotlinx.css.*
 import react.*
 import react.dom.h1
 import react.dom.h3
-import react.dom.img
-import react.dom.select
-import styled.css
-import styled.styledDiv
 
 external interface AppState : RState {
     var currentVideo: Video?
+    var unwatchedVideos: List<Video>
+    var watchedVideos: List<Video>
 }
+
 
 @JsExport
 class App : RComponent<RProps, AppState>() {
+    override fun AppState.init() {
+        unwatchedVideos = listOf(
+            KotlinVideo(
+                1,
+                "Building and breaking things",
+                "John Doe",
+                "https://youtu.be/PsaFVLr8t4E"
+            ),
+            KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+            KotlinVideo(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+        )
+        watchedVideos = listOf(
+            KotlinVideo(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+        )
+    }
+
     override fun RBuilder.render() {
         h1 {
             +"KotlinConf Explorer"
@@ -23,7 +37,7 @@ class App : RComponent<RProps, AppState>() {
             //     attrs.videos=unwatchedVideos
             // }
             videoList {
-                videos = unwatchedVideos
+                videos = state.unwatchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video ->
                     setState { currentVideo = video }
@@ -36,27 +50,47 @@ class App : RComponent<RProps, AppState>() {
             //     attrs.videos=watchedVideos
             // }
             videoList {
-                videos = watchedVideos
+                videos = state.watchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video -> setState { currentVideo = video } }
 
             }
         }
 
-        styledDiv {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
+        // styledDiv {
+        //    css {
+        //         position = Position.absolute
+        //          top = 10.px
+        //          right = 10.px
 
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
+        //      }
+        //       h3 {
+        //          +"John Doe: Building and breaking things"
+        //      }
 
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+        //       img {
+        //           attrs {
+        //               src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+        //            }
+        //        }
+        //    }
+        state.currentVideo?.let { currentVideo ->
+            videoPlayer {
+                video = currentVideo
+                unWatchedVideo = currentVideo in state.unwatchedVideos
+                onWatchedButtonPressed = {
+                    if (video in state.unwatchedVideos) {
+                        setState {
+                            unwatchedVideos -= video
+                            watchedVideos += video
+                        }
+
+                    } else {
+                        setState {
+                            watchedVideos -= video
+                            unwatchedVideos += video
+                        }
+                    }
                 }
             }
         }
---
+++ src/main/kotlin/VideoPlayer.kt
@@ -0,0 +1,57 @@
+import kotlinx.css.*
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.h3
+import react.dom.img
+import styled.css
+import styled.styledButton
+import styled.styledDiv
+
+external interface VideoPlayerProps: RProps{
+    var video: Video
+    var onWatchedButtonPressed: (Video) -> Unit
+    var unWatchedVideo: Boolean
+}
+@JsExport
+class VideoPlayer: RComponent<VideoPlayerProps,RState>(){
+    override fun RBuilder.render(){
+        styledDiv {
+            css {
+                position=Position.absolute
+                top=10.px
+                right=10.px
+
+            }
+            h3 {
+                +"${props.video.speaker}: ${props.video.title}"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+        styledButton {
+            css {
+                display=Display.block
+                backgroundColor=if(props.unWatchedVideo) Color.lightGreen else Color.red
+            }
+            attrs {
+                onClickFunction={
+                    props.onWatchedButtonPressed(props.video)
+                }
+                if(props.unWatchedVideo){
+                    +"Mark as watched"
+                }
+                else{
+                    +"Mark as unwatched"
+                }
+            }
+        }
+    }
+}
+fun RBuilder.videoPlayer(handler: VideoPlayerProps.() -> Unit): ReactElement{
+    return child(VideoPlayer::class){
+        this.attrs(handler)
+    }
+}
